4cd7df3817d27a92155b9cebc5fdff959a3dc969
chauhan_Parikshit <chauhanthe2nd@gmail.com>
2020-08-17 01:11:10 +0530

    another commit

--- src/main/java/demo/oauthTest.java
+++ src/main/java/demo/oauthTest.java
@@ -2,24 +2,35 @@ package demo;
 
 import static io.restassured.RestAssured.given;
 
-public class oauthTest {
-	
-		public static void main(String args[]) {
-			
-			given()
-			.queryParams("code","")
-			.queryParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
-			.queryParams("client_secret","erZOWM9g3UtwNRj340YYaK_W")
-			
-			
-			
-		String response  = 	given().queryParam("access_token", "")
-			.when()
-			.get("https://rahulshettyacademy.com/getCourse.php")
-			.asString();
-		
-		System.out.println(response);
-		}
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.restassured.path.json.JsonPath;
+
+public class oauthTest {
+
+	public static void main(String args[]) throws InterruptedException {
+		String url=;
+		String partialcode=url.split("code=")[1];
+		String code=partialcode.split("&scope")[0];
+		System.out.println(code);
+
+		String accessTokenResponse = given().queryParams("code", "")
+				.queryParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+				.queryParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+				.queryParams("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
+				.queryParams("grant_type", "authorization_code").when().log().all()
+				.post("https://www.googleapis.com/oauth2/v4/token").asString();
+
+		JsonPath js = new JsonPath(accessTokenResponse);
+		String accessToken = js.getString("access_token");
+
+		String response = given().queryParam("access_token", accessToken).when()
+				.get("https://rahulshettyacademy.com/getCourse.php").asString();
+
+		System.out.println(response);
+	}
 
 }
